44d904023fe28109d26710c1d7c1840114c5b9e8
Corey Goldberg <cgoldberg@gmail.com>
2013-10-29 15:51:32 -0400

    refactoring

--- easymiv.py
+++ easymiv.py
@@ -24,93 +24,44 @@ from PIL import Image
 from PIL.ImageTk import PhotoImage
 
 
-class Config:
-    def __init__(self):
-        self.detect_images = True
-        self.auto_slide_time = 3000
-        self.auto_slide_auto_start = False
-
-
 class SlideShow:
-    def __init__(self, input_path):
-        self.current_index = -1
-        self.startcurrent_index = -1
-        self.input_path = input_path
-        self.input_dir = self.input_path
-        input_file = None
-        if os.path.isfile(self.input_path):
-            self.input_dir, input_file = os.path.split(self.input_path)
-        self.current_index = 0
-        all_data = sorted(os.listdir(self.input_dir))
-        if ((all_data is None) or (len(all_data) <= 0)):
-            return
-        self.files = []
-        for f in all_data:
-            tf = os.path.join(self.input_dir, f)
-            if os.path.isfile(tf):
-                can_add = True
-                if config.detect_images:
-                    try:
-                        Image.open(tf)
-                    except Exception:
-                        can_add = False
-                if not can_add:
-                    continue
-                self.files.append(tf)
-                if ((input_file is not None) and (input_file == f)):
-                    self.current_index = len(self.files) - 1
-        self.startcurrent_index = self.current_index
 
-    def has_files(self):
-        return ((self.files is not None) and (len(self.files) > 0))
+    def __init__(self, input_dir):
+        self.current_index = 0
+        img_paths = []
+        for root, dirs, files in os.walk(input_dir, topdown=True):
+            for file in sorted(files):
+                img_paths.append(os.path.abspath(os.path.join(root, file)))
+        self.files = img_paths
 
     def get_current(self):
-        if not self.has_files():
-            raise StopIteration
         return self.files[self.current_index]
 
     def get_extra(self):
-        return '[%d / %d]  ' % (self.current_index + 1, len(self.files))
+        return '%d / %d' % (self.current_index + 1, len(self.files))
 
     def move_next(self):
-        if not self.has_files():
-            raise StopIteration
         self.current_index += 1
         if self.current_index >= len(self.files):
             self.current_index = 0
         return self.get_current()
 
     def move_previous(self):
-        if not self.has_files():
-            raise StopIteration
         self.current_index -= 1
         if self.current_index < 0:
             self.current_index = len(self.files) - 1
         return self.get_current()
 
-    def move_first(self, first):
-        if not self.has_files():
-            raise StopIteration
-        self.current_index = 0
-        if not first:
-            self.current_index = len(self.files) - 1
-        return self.get_current()
-
 
 class Display:
+
     def __init__(self, root):
         self.root = root
-        self.masterimage = None
-        self.photoimage = None
-        self.image_id = -1
-        self.text_id = -1
-        self.current_file = None
+        self.image_id = None
+        self.text_id = None
         self.display = Canvas(
             root, bg='black', borderwidth=0, highlightthickness=0)
         self.display.pack(expand=YES, fill=BOTH)
-
-    def has_valid_image(self):
-        return (self.photoimage is not None)
 
     def _zoom_image(self, img, w, h):
         original_width = img.size[0]
@@ -118,33 +69,28 @@ class Display:
         img = img.resize(
             (int(img.size[0] * r), int(img.size[1] * r)), Image.BILINEAR)
         current_zoom = int((float(img.size[0]) / original_width) * 100)
-        text = '%s%%' % current_zoom
-        return img, text
+        zoom_text = '%s%%' % current_zoom
+        return img, zoom_text
 
-    def set_image(self, file, extra, reload_image=True):
-        self.photoimage = None
-        self.masterimage = None
-        if file is None:
-            return
+    def set_image(self, file, extra):
         self.current_file = file
-        text = os.path.basename(file)
-        text = extra + text
-        if ((self.masterimage is None) or reload_image):
-            self.masterimage = Image.open(file)
-        w, h = self.display.winfo_width(), self.display.winfo_height()
+        w = self.display.winfo_width()
+        h = self.display.winfo_height()
+        self.masterimage = Image.open(file)
         img, zoom_text = self._zoom_image(self.masterimage, w, h)
-        size = '%sx%s' % self.masterimage.size
-        text += '  [%s]  %s' % (size, zoom_text)
         self.photoimage = PhotoImage(img)
-        if self.image_id < 0:
+        size = '%sx%s' % self.masterimage.size
+        text = '[%s]  %s  (%s)  %s' % (
+            extra, os.path.basename(file), size, zoom_text)
+
+        if self.image_id is None:
             self.image_id = self.display.create_image(0, 0)
         self.display.coords(self.image_id, w // 2, h // 2)
         self.display.itemconfig(
             self.image_id, image=self.photoimage, anchor=CENTER)
-        self.has_image = True
 
         # set text
-        if self.text_id < 0:
+        if self.text_id is None:
             self.text_id = self.display.create_text(5, 5)
         self.display.itemconfig(self.text_id, text=text, anchor=NW, fill='red')
         self.display.lift(self.text_id)
@@ -152,88 +98,67 @@ class Display:
 
 class Application:
 
-    def __init__(self, root):
-        self.auto_slide_on = False
-        self.mstart = (-1, -1)
-        self.slide = None
+    def __init__(self, root, auto_slide_on=False, auto_slide_time=3000):
         self.root = root
+        self.auto_slide_on = False
+        self.auto_slide_time = auto_slide_time
         self.root.config(borderwidth=0, cursor='none')
-        self.display = Display(root)
         self.root.bind('q', lambda e: self.quit())
         self.root.bind('<Escape>', lambda e: self.quit())
         self.root.bind('<Control-c>', lambda e: self.quit())
-        self.root.bind('<space>', lambda e: self.show_next(e, True))
-        self.root.bind('<Right>', lambda e: self.show_next(e, True))
-        self.root.bind('<Down>', lambda e: self.show_next(e, True))
-        self.root.bind('<Return>', lambda e: self.show_next(e, True))
-        self.root.bind('<Left>', lambda e: self.show_next(e, False))
-        self.root.bind('<Up>', lambda e: self.show_next(e, False))
+        self.root.bind('<space>', lambda e: self.show_next())
+        self.root.bind('<Right>', lambda e: self.show_next())
+        self.root.bind('<Down>', lambda e: self.show_next())
+        self.root.bind('<Return>', lambda e: self.show_next())
+        self.root.bind('<Left>', lambda e: self.show_next(False))
+        self.root.bind('<Up>', lambda e: self.show_next(False))
         self.root.bind('s', lambda e: self.auto_slide())
         # bind function keys: F1-F12
         for i in range(12):
             funtion_key_label = '<F%d>' % (i + 1)
             self.root.bind(funtion_key_label, lambda e: self.auto_slide())
+        self.display = Display(self.root)
 
-    def run(self, input_path):
-        w, h = self.root.winfo_screenwidth(), self.root.winfo_screenheight()
+    def run(self, input_dir):
+        w = self.root.winfo_screenwidth()
+        h = self.root.winfo_screenheight()
         self.root.geometry('%dx%d+0+0' % (w, h))
         self.root.attributes('-fullscreen', True)
 
-        self.input_path = input_path
-        self._init_files(input_path)
+        self.slide = SlideShow(input_dir)
+        self.root.after(150, lambda: self.show_current())
+
         self.root.focus_set()
         self.root.focus_force()
         self.root.mainloop()
 
-    def _init_files(self, input_path):
-        self.slide = SlideShow(input_path)
-        self.root.after(150, lambda: self.show_current())
-
-    def show_next(self, e, down):
-        if self.slide is None:
-            return
-        if down:
+    def show_next(self, forward=True):
+        if forward:
             self.slide.move_next()
         else:
             self.slide.move_previous()
         self.show_current()
 
     def show_current(self):
-        global config
-        if self.slide is None:
-            return
         self.display.set_image(
-            self.slide.get_current(), self.slide.get_extra(), True)
-        if config.auto_slide_auto_start:
-            config.auto_slide_auto_start = False
-            self.root.after(config.auto_slide_time, lambda: self.auto_slide())
+            self.slide.get_current(), self.slide.get_extra())
+        if self.auto_slide_on:
+            self.root.after(self.auto_slide_time, lambda: self.auto_slide())
 
     def auto_slide(self):
-        self.auto_slide_on = True
-        self.show_next(None, True)
-        self.root.after(config.auto_slide_time, lambda: self.auto_slide_next())
-
-    def auto_slide_next(self):
-        self.show_next(None, True)
-        self.root.after(config.auto_slide_time, lambda: self.auto_slide_next())
-
-    def auto_slide_stop(self):
-        self.auto_slide_on = False
+        self.show_next()
+        self.root.after(self.auto_slide_time, lambda: self.auto_slide())
 
     def quit(self):
         self.root.destroy()
 
 
 if __name__ == '__main__':
-    global config
     root = Tk()
-    config = Config()
     parser = argparse.ArgumentParser()
     parser.add_argument('dir')
     parser.add_argument(
         '-s', '--slideshow', help='start slideshow', action='store_true')
     args = parser.parse_args()
-    if args.slideshow:
-        config.auto_slide_auto_start = True
-    app = Application(root)
+    app = Application(root, args.slideshow, 3000)
     app.run(args.dir)
